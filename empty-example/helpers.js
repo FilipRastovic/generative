d0fc49b2a29a4beb704b7caadeffbb9e67a4c1e2
filiporastovic@gmail.com <filiporastovic@gmail.com>
2018-11-19 15:31:13 +0100

    -oop updates

--- empty-example/helpers.js
+++ empty-example/helpers.js
@@ -8,8 +8,39 @@ function hexagon (posX, posY, radius) {
   endShape(CLOSE)
 }
 
+// Drawing a polygon at x,y, with radius r
+function polygon(x,y,r,noOfSides1){
+  push();
+   // translate(width/2,height/2);
+   // ellipse(x,y,2*r,2*r); // (for reference)
+   // noFill();
+   let theta = 360/noOfSides1;
+    beginShape()
+     for(let i = 0; i<noOfSides1; i++){
+      vertex(x + r*cos(theta*i),y + r*sin(theta*i));
+     }
+    endShape(CLOSE);
+  pop();
+ }
+
+
+
 function pointOnCircle (posX, posY, radius, angle) {
   const x = posX + radius * cos(angle)
   const y = posY + radius * sin(angle)
   return createVector(x, y)
+}
+
+function randomSelectTwo() {
+  const rando = random(1)
+  if (rando > 0.5) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function getRandomFromPalette () {
+  const rando2 = floor(random(0, PALETE.length))
+  return PALETE[rando2]
 }
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -10,13 +10,56 @@ function setup() {
 
   PALETE = [
     color(190, 190, 190), //pink
-    color(120, 120, 120)
+    color(220, 220, 220)
   ]
 }
 
 function draw() {
-  //testLines()
-  outlineShape()
+  const layer = new Circles()
+  console.log(layer)
+  layer.render()
+  // testLines()
+  // outlineShape()
+  // simpleLines()
+  // circle()
+
+  // let picker = random(1)
+  // if (picker > 0.3) {
+  //   outlineShape()
+  // }
+
+  // picker = random(1)
+  // if(picker > 0.3) {
+  //   simpleLines()
+  // }
+
+  // picker = random(1)
+  // if (picker > 0.3) {
+  //   circles()
+  // }
+}
+
+function simpleLines () {
+  const stepsOut = 8
+  const numSteps = randomSelectTwo() ? stepsOut : int(stepsOut * 1.25)
+  const step = (CRYSTAL_SIZE / 2) / numSteps
+  const start = floor(random(0, numSteps))
+  const stop = floor(random(start, numSteps + 1))
+
+  let numShapes = randomSelectTwo() ? SIDES : SIDES * 2
+  const strokeColor = getRandomFromPalette();
+  const angle = 360 / numShapes
+  const weight = randomSelectTwo() ? 1 : 3
+  noFill()
+  stroke(strokeColor)
+  strokeWeight(weight)
+  push()
+    translate(width/2, height/2)
+    for (let i = 0; i < numShapes; i ++) {
+      line(start * step, 0, stop * step, 0)
+      rotate(angle)
+    }
+  pop()
 }
 
 function outlineShape () {
@@ -53,18 +96,4 @@ function testLines () {
       rotate(angle)
     }
   pop()
-}
-
-function randomSelectTwo() {
-  const rando = random(1)
-  if (rando > 0.5) {
-    return true
-  } else {
-    return false
-  }
-}
-
-function getRandomFromPalette () {
-  const rando2 = floor(random(0, PALETE.length))
-  return PALETE[rando2]
 }
