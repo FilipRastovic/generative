5ff05ba130c7e967752539c2505913c3e3a38a16
filiporastovic@gmail.com <filiporastovic@gmail.com>
2018-12-08 22:44:26 +0100

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,9 +1,21 @@
-const CRYSTAL_SIZE = 500
+const CRYSTAL_SIZE = 150
 const SIDES = 6
+
+// layout
+const MARGIN = CRYSTAL_SIZE / 2
+const COLUMNS = 3
+const ROWS = 4
+const PADDING = CRYSTAL_SIZE * 0.2
+const GRIDBOX = CRYSTAL_SIZE + PADDING
+const START = (CRYSTAL_SIZE / 2) + MARGIN
+
 let PALETTE = []
+ALL_CRYSTALS = []
 
 function setup() {
-  createCanvas(530, 530, SVG)
+  const totalX = START + GRIDBOX * COLUMNS
+  const totalY = START + GRIDBOX * ROWS
+  createCanvas(totalX, totalY, SVG)
 
   PALETTE = [
     color(255, 52, 154), // pink
@@ -16,12 +28,19 @@ function setup() {
 }
 
 function draw() {
-
-  // go to a point on the screen and draw a pixel
+  // go to a point on the screen and draw a crystal
   // continue to do this until we run out of room
+  for (let x = 0; x < COLUMNS; x++) {
+    for (let y = 0; y < ROWS; y++) {
+      const posX = START + (x * GRIDBOX)
+      const posY = START + (y * GRIDBOX)
+      ALL_CRYSTALS.push(new Crystal(posX, posY))
+    }
+  }
 
-  const oneCrystal = new Crystal(width/2, height/2)
-  oneCrystal.render()
+  ALL_CRYSTALS.forEach(crystal => {
+    crystal.render()
+  })
 }
 
 
